648c6fa77426517545594bcc5732dbf52e15c54c
dieterjava <md_hermann284%%>
2021-06-10 09:04:29 +0200

    Added promise

--- README.txt
+++ README.txt
@@ -39,7 +39,6 @@ e.g. bootstrap, be aware of the version incompatibilities, 3...., 4..., 5....
 To Dos:
 
 0. Good: only one javascript file.
-2. use Promise = fetch ..., easier for debugging.
 3. No extra libraries, remove bootstrap
 4. Add new field
 4. Minimize fields occurences, for generic javascript generation.
--- main.js
+++ main.js
@@ -1,25 +1,27 @@
 /*
-*    Javascript Code , Web Components
-*   for a POST, PATCH, DELETE example
-*/
+ *    Javascript Code , Web Components
+ *   for a POST, PATCH, DELETE example
+ */
 let postsList = document.querySelector(".posts-list");
 let addPostForm = document.querySelector(".add-post-form");
-const titleValue = document.getElementById('id_title');
-const contentValue = document.getElementById('id_content');
-const btnSubmit = document.querySelector('.btn');
+const titleValue = document.getElementById("id_title");
+const contentValue = document.getElementById("id_content");
+const btnSubmit = document.querySelector(".btn");
 
 let url = "http://localhost:3000/posts";
 
 let output = "";
 
-// initial fetch 
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => renderPosts(data))
+// initial fetch
+const getPromise = fetch(url, { method: "GET" });
 
-  const renderPosts = (posts) => {
-    posts.forEach((post) => {
-      output += `
+getPromise
+  .then((response) => response.json())
+  .then((data) => renderPosts(data));
+
+const renderPosts = (posts) => {
+  posts.forEach((post) => {
+    output += `
         <div class="card mt-4 col-md-6 bg-light">
         <div class="card-body" data-id=${post.id}>
           <h5 class="card-title">${post.title}</h5>
@@ -29,84 +31,80 @@ fetch(url)
           <a href="#" class="card-link" id="id_delete">Delete</a>
         </div>
       </div> `;
-      // console.log(post.content);
+    // console.log(post.content);
+  });
+  postsList.innerHTML = output;
+  // console.log(postsList);
+};
+
+// listener for submit , new and edit, POST and PATCH
+addPostForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("POST , ....Form submitted!");
+
+  const postPromise = fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title: titleValue.value,
+      content: contentValue.value,
+    }),
+  });
+  postPromise
+    .then((response) => response.json())
+    .then((data) => {
+      const dataArray = [];
+      dataArray.push(data);
+      renderPosts(dataArray);
     });
-    postsList.innerHTML = output;
-   // console.log(postsList);
-  }
-
-  // listener for submit , new and edit, POST and PATCH
-  addPostForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    console.log('POST , ....Form submitted!');
-
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        title: titleValue.value,
-        content: contentValue.value
-      })
-
-    })
-      .then(response => response.json())
-      .then(data =>{
-        const dataArray = [];
-        dataArray.push(data);
-        renderPosts(dataArray);
-      }) 
-})
+});
 
 // event listener for edit and delete
-postsList.addEventListener('click', (e) =>{
- // console.log(e.target.id);
- e.preventDefault();
- let editButtonIsPressed = e.target.id == "id_edit";
- let deleteButtonIsPressed = e.target.id == "id_delete";
- 
-// console.log(e.target.parentElement.dataset.id);
+postsList.addEventListener("click", (e) => {
+  // console.log(e.target.id);
+  e.preventDefault();
+  let editButtonIsPressed = e.target.id == "id_edit";
+  let deleteButtonIsPressed = e.target.id == "id_delete";
 
-let id = e.target.parentElement.dataset.id;
+  // console.log(e.target.parentElement.dataset.id);
 
-if (deleteButtonIsPressed){
-  fetch( `${url}/${id}`, 
-  {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json'}
-    })
-      .then(result => result.json())
-      .then(() => location.reload());
+  let id = e.target.parentElement.dataset.id;
+
+  if (deleteButtonIsPressed) {
+    const deletePromise = fetch(`${url}/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    deletePromise.then((result) => result.json()).then(() => location.reload());
   }
 
-
-  if (editButtonIsPressed){
+  if (editButtonIsPressed) {
     const parent = e.target.parentElement;
-    let title = parent.querySelector('.card-title').textContent;
-    let content =  parent.querySelector('.card-text').textContent;
+    let title = parent.querySelector(".card-title").textContent;
+    let content = parent.querySelector(".card-text").textContent;
 
     titleValue.value = title;
     contentValue.value = content;
 
-    btnSubmit.addEventListener('click', (event) => {
+    btnSubmit.addEventListener("click", (event) => {
       event.preventDefault();
-      console.log('calling PATCH');
+      console.log("calling PATCH");
 
-      fetch( `${url}/${id}`, 
-      {
-        method: 'PATCH',
-        headers: { 'Content-Type': 'application/json'},
+      const patchPromise = fetch(`${url}/${id}`, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-            title: titleValue.value,
-            content: contentValue.value
-        })
-        })
-          .then(result => result.json())
-          .then(() => location.reload());
-
-    })
+          title: titleValue.value,
+          content: contentValue.value,
+        }),
+      });
+      patchPromise
+        .then((result) => result.json())
+        .then(() => location.reload());
+    });
   }
-})
-
+});
